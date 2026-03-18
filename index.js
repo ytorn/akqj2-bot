e1fc4701a26258c64d49ce19d146d5c2f90b3a4a
ytorn <mr.pituley@gmail.com>
2026-03-18 12:07:24 +0200

    cors middleware added

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ import {declineChipRequest} from "./actions/declineChipRequest.js";
 import {schedulePost} from "./cronJobs/schedulePost.js";
 import {requestChips} from "./scenes/requestChips.js";
 import {apiRouter} from "./routes/api.js";
+import cors from 'cors';
 
 dayjs.extend(utc)
 
@@ -38,6 +39,14 @@ if (!fs.existsSync(logsDir)) {
 }
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
+
 app.use(express.json());
 
 const adminUsers = {};
