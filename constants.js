f1affbf42766e2e73e23c053c5cdd2f1817bbfeb
ytorn <mr.pituley@gmail.com>
2026-08-26 01:07:36 +0300

    admins removed

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 export const ADMIN_ID = 323046603; // JP
 export const ADMIN2_ID = 561292215; // AH
-export const ADMIN3_ID = 564354756; // KT
-export const ADMIN4_ID = 143324479; // AB
+// export const ADMIN3_ID = 564354756; // KT
+// export const ADMIN4_ID = 143324479; // AB
